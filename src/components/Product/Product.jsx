948d3b322f0f66c1ab199b1b510cb5312c937297
ZILLUR KHAN <masudcse9292@gmail.com>
2023-03-28 01:00:15 +0600

    Add to cart function add

---
+++ src/components/Product/Product.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import './Product.css';
+
+const Products = (props) => {
+    const { img, name, seller, ratings, quantity, price } = props.product;
+
+    const handleAddToCart = props.handleAddToCart;
+
+    // console.log(props.product)
+
+
+    return (
+        <div className='product'>
+            <img src={img} alt="" />
+            <div className='product-info'>
+                <h6> {name}</h6>
+                <p>Price: ${price}</p>
+                <p>Manufacturer: {seller}</p>
+                <p>Rating {ratings}</p>
+            </div>
+            <button onClick={() => handleAddToCart(props.product)} className='btn-cart'>Add to Cart</button>
+        </div>
+    );
+};
+
+export default Products;
